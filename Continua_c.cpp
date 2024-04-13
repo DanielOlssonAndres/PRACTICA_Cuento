d904ed8d3acaecf1a7a9b65d563197cf2c5b1215
DanielOlssonAndres <99800510+DanielOlssonAndres@users.noreply.github.com>
2024-04-13 16:55:05 +0200

    puuuu
    si

--- Continua_c.cpp
+++ Continua_c.cpp
@@ -2,15 +2,12 @@
 #include<stdio.h>
 
 int main(){
+    int i = 0;
     printf("se tropezó con una piedra");
+    i++;
     std::cout << "en verdad no, era broma" << endl;
     return 0;
 }
-=======
-#include<stdio.h>
 
-int main(){
-    printf("se tropezó con una piedra");
-    return 0;
-}
+
 
